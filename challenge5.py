e538acf14702e1b80fd31f062480d99a6802b6a6
Alvin Vidad <alvin.vidad@gmail.com>
2019-05-16 12:21:03 -0400

    day4

---
+++ challenge5.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import random
+
+def thursday ():
+    for x  in range (10000)
+    thursday.append(random.randint (1, 600)
+
+print (thursday)
--- iptaker01.py
+++ iptaker01.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
+"""This is an input program"""
 
-user_input = input ("Please enter an IPv4 IP address: ")
+USER_INPUT = input("Please enter an IPv4 IP address: ")
 
-print ("You told me the IPv4 address is: " + user_input)
+print("You told me the IPv4 address is: " + USER_INPUT)
